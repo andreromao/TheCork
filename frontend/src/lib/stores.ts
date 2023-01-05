9bbf713163062046ce1dcd2b74e4c78f27237cc6
rodrigohpalmeirim <rodrigohpalmeirim@hotmail.com>
2023-01-05 04:05:41 +0000

    add automatic token refresh

--- frontend/src/lib/stores.ts
+++ frontend/src/lib/stores.ts
@@ -1,9 +1,35 @@
 import { writable } from 'svelte/store'
 import { browser } from '$app/environment'
+import { AUTH_URL } from '$env/static/public'
+import { goto } from '$app/navigation';
 
 let persistedUser = browser && localStorage.getItem('user')
 export let user = writable(persistedUser ? JSON.parse(persistedUser) : '')
+let userValue;
+user.subscribe(u => userValue = u);
 
 if (browser) {
     user.subscribe(u => localStorage.user = JSON.stringify(u))
+    if (persistedUser) refresh();
+    setInterval(refresh, 1000 * 60 * 2);
 }
+
+async function refresh() {
+    if (!userValue) return;
+    const res = await fetch(`${AUTH_URL}/token`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            token: userValue.refreshToken,
+            username: userValue.username,
+        }),
+    });
+    
+    if (res.ok) {
+        return { ...userValue, accessToken: await res.text() };
+    } else {
+        goto('/login');
+    }
+};
