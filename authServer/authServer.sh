ed819eee13457f36e4bfcf6b8430ecff6750a594
André Romão <andre.miguel01@gmail.com>
2023-01-05 23:20:55 +0000

    simple script

--- authServer/authServer.sh
+++ authServer/authServer.sh
@@ -1,7 +1,7 @@
 !#/bin/bash
 apt update -y 
 apt install npm -y
-apt install wget curl gnupg2 software-properties-common apt-transport-https ca-certificates lsb-release
+apt install wget curl gnupg2 software-properties-common apt-transport-https ca-certificates lsb-release -y
 curl -fsSL https://www.mongodb.org/static/pgp/server-6.0.asc -y | gpg --dearmor -o /etc/apt/trusted.gpg.d/mongodb-6.gpg
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
 wget http://archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1f-1ubuntu2.16_amd64.deb
