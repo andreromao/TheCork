ecb86f43e95755e352ceabab7269f2e51eb749d8
André Romão <andre.miguel01@gmail.com>
2023-01-06 14:43:23 +0000

    changes

--- installScripts/router1.sh
+++ installScripts/router1.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Change firewall and netplan files
-sudo cp -f scriptFiles/00-installer-config.yaml /etc/netplan/00-installer-config.yaml
-sudo cp -f scriptFiles/before.rules /etc/ufw/before.rules
-sudo cp -f scriptFiles/defufw /etc/default/ufw
-sudo cp -f scriptFiles/user.rules /etc/ufw/user.rules
-sudo cp -f scriptFiles/sysctl.conf /etc/ufw/sysctl.conf
+sudo cp -f routerScriptFiles/00-installer-config.yaml /etc/netplan/00-installer-config.yaml
+sudo cp -f routerScriptFiles/before.rules /etc/ufw/before.rules
+sudo cp -f routerScriptFiles/defufw /etc/default/ufw
+#sudo cp -f routerScriptFiles/user.rules /etc/ufw/user.rules
+sudo cp -f routerScriptFiles/sysctl.conf /etc/ufw/sysctl.conf
 
 netplan apply 
 ufw enable
